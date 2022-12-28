4399ba613cf77e0d24037db06f234a6e8e432ea8
Nopock <78834703+Nopock@users.noreply.github.com>
2022-12-28 07:42:45 -0800

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.22"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+group = "org.hyrical"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.aikar.co/content/groups/aikar/")
+    maven("https://jitpack.io")
+    maven("https://repo.lucko.me/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    implementation("co.aikar:acf-paper:0.5.1-SNAPSHOT")
+
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+
+    implementation("com.github.Nopock:Store:5a04316035")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
